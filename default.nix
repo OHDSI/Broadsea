afa019a14f449eb92616f746a7ed137eff67fdcb
haydenbspence <hayden@spence.cloud>
2023-07-09 13:37:00 -0400

    modified default.nix

--- default.nix
+++ default.nix
@@ -1,24 +1,10 @@
-{ stdenv, fetchurl }:
+{ pkgs ? import <nixpkgs> { }, pythonPackages ? pkgs.python3Packages }:
 
-stdenv.mkDerivation rec {
-  name = "hello-2.10";
+pkgs.mkShell {
+  buildInputs = [
+     pythonPackages.numpy
+     pythonPackages.scipy
+     pythonPackages.jupyterlab
+  ];
 
-  src = fetchurl {
-    url = "mirror://gnu/hello/${name}.tar.gz";
-    sha256 = "0ssi1wpaf7plaswqqjwigppsg5fyh99vdlb9kzl7c9lng89ndq1i";
-  };
-
-  doCheck = true;
-
-  meta = {
-    description = "A program that produces a familiar, friendly greeting";
-    longDescription = ''
-      GNU Hello is a program that prints "Hello, world!" when you run it.
-      It is fully customizable.
-    '';
-    homepage = http://www.gnu.org/software/hello/manual/;
-    license = stdenv.lib.licenses.gpl3Plus;
-    maintainers = [ stdenv.lib.maintainers.eelco ];
-    platforms = stdenv.lib.platforms.all;
-  };
 }
