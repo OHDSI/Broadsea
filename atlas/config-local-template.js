780f3a6b5d4dbfbb7c9af8c547160c4277ed1e8a
Lee Evans <leeevans@users.noreply.github.com>
2023-09-05 11:08:35 -0400

    fix old Broadsea 2.0 code causing bug (#106)
    fix old Broadsea 2.0 bug - causing random logouts

--- atlas/config-local-template.js
+++ atlas/config-local-template.js
@@ -1,9 +1,6 @@
 define([], function () {
 	var configLocal = {};
 
-	// clearing local storage otherwise source cache will obscure the override settings
-	localStorage.clear();
-
 	var getUrl = window.location;
 	var baseUrl = getUrl.protocol + "//" + getUrl.host;
 	
