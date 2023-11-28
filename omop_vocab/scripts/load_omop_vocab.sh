0bc89b176c38cd2308b545d2e606101b7ebecc7d
alondhe <londhe@ohdsi.org>
2023-11-28 09:35:29 -0600

    Change from < to cat in omop vocab script

--- omop_vocab/scripts/load_omop_vocab.sh
+++ omop_vocab/scripts/load_omop_vocab.sh
@@ -6,7 +6,7 @@ apk add postgresql-client
 
 cd /tmp/files
 
-UMLS_API_KEY=$(</run/secrets/UMLS_API_KEY)
+UMLS_API_KEY=$(cat /run/secrets/UMLS_API_KEY)
 
 if [ -z "$UMLS_API_KEY" ]
 then
@@ -19,18 +19,18 @@ else
     sh ./cpt.sh $UMLS_API_KEY
 fi
 
-PGPASSWORD=$(</run/secrets/VOCAB_PG_PASSWORD) psql -v vocab_schema=$VOCAB_PG_SCHEMA -h $VOCAB_PG_HOST -U $VOCAB_PG_USER -d $VOCAB_PG_DATABASE -a -f /tmp/scripts/omop_vocab_ddl.sql
+PGPASSWORD=$(cat /run/secrets/VOCAB_PG_PASSWORD) psql -v vocab_schema=$VOCAB_PG_SCHEMA -h $VOCAB_PG_HOST -U $VOCAB_PG_USER -d $VOCAB_PG_DATABASE -a -f /tmp/scripts/omop_vocab_ddl.sql
 
 tables="DRUG_STRENGTH CONCEPT_SYNONYM CONCEPT_ANCESTOR CONCEPT_CLASS CONCEPT_RELATIONSHIP CONCEPT DOMAIN RELATIONSHIP VOCABULARY"
 
 for table in $tables
 do
     echo 'Now loading: ' $table
-    PGPASSWORD=$(</run/secrets/VOCAB_PG_PASSWORD) psql -h $VOCAB_PG_HOST -d $VOCAB_PG_DATABASE -U $VOCAB_PG_USER \
+    PGPASSWORD=$(cat /run/secrets/VOCAB_PG_PASSWORD) psql -h $VOCAB_PG_HOST -d $VOCAB_PG_DATABASE -U $VOCAB_PG_USER \
         -c "\COPY $VOCAB_PG_SCHEMA.$table FROM /tmp/files/$table.csv (DELIMITER E'\t', FORMAT CSV, NULL '""', QUOTE E'\b', HEADER, ENCODING 'UTF8')"
     echo 'Finished loading: ' $table
 done
 
-PGPASSWORD=$(</run/secrets/VOCAB_PG_PASSWORD) psql -v vocab_schema=$VOCAB_PG_SCHEMA -h $VOCAB_PG_HOST -U $VOCAB_PG_USER -d $VOCAB_PG_DATABASE -a -f /tmp/scripts/omop_vocab_indices.sql
+PGPASSWORD=$(cat /run/secrets/VOCAB_PG_PASSWORD) psql -v vocab_schema=$VOCAB_PG_SCHEMA -h $VOCAB_PG_HOST -U $VOCAB_PG_USER -d $VOCAB_PG_DATABASE -a -f /tmp/scripts/omop_vocab_indices.sql
 
 echo 'All done, shutting down. Feel free to remove container.'
